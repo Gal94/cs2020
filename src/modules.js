be0f2586aad8a79ab0ba8f66e3b42ff8c3b974d7
Gal94 <gal.yacobi8@gmail.com>
2019-10-03 21:13:17 +0300

    added drive links

--- src/modules.js
+++ src/modules.js
@@ -2,47 +2,47 @@ export const firstYearModules = [
     {
         id: 1,
         name: 'אינפי 1',
-        link: ''
+        link: 'https://mega.nz/#F!o9RxFQwA!8rIZ9PZQ1Xygd9QmrvJBbw'
     },
     {
         id: 2,
         name: 'אלגברה לינארית',
-        link: ''
+        link: 'https://mega.nz/#F!d8IVhAyQ!hE-87pRO_kWu4MTdwvTuGQ'
     },
     {
         id: 3,
         name: 'מתמטיקה בדידה',
-        link: ''
+        link: 'https://mega.nz/#F!Y9ZHjYCJ!BhNpDPdKMnJMlP3yodRIwA'
     },
     {
         id: 4,
         name: 'מבוא למדעי המחשב',
-        link: ''
+        link: 'https://mega.nz/#F!U1BlyQbR!GdkazmseuOcjmNyqHDFHCA'
     },
     {
         id: 5,
         name: 'אינפי 2',
-        link: ''
+        link: 'https://mega.nz/#F!x8Bx2I7Y!9cV0rvRo8wfn0d2dVxcb0A'
     },
     {
         id: 6,
         name: 'מבני נתונים',
-        link: ''
+        link: 'https://mega.nz/#F!1pJBEIaR!N9iLpXZyZfzGAmyiNtET2g'
     },
     {
         id: 7,
         name: 'סדנה מתקדמת בתכנות',
-        link: ''
+        link: 'https://mega.nz/#F!E0BT1AAR!I1o6kjUaiE_CG2CHOQ4rJg'
     },
     {
         id: 8,
         name: 'מיתוג',
-        link: ''
+        link: 'https://mega.nz/#F!clIRharb!NsVq2eRxm1nndYXe5d3rVQ'
     },
     {
         id: 9,
         name: 'הסתברות',
-        link: ''
+        link: 'https://mega.nz/#F!BsAhWKbY!qH4Ei99k9rWVA6GX1VR7mA'
     },
 ];
 
@@ -50,52 +50,52 @@ export const secondYearModules = [
     {
         id: 1,
         name: 'בסיסי נתונים',
-        link: ''
+        link: 'https://mega.nz/#F!E4I3ECwa!214Tg2HyHGetqL-t9v8hig'
     },
     {
         id: 2,
         name: 'אסמבלי ושפת סף',
-        link: ''
+        link: 'https://mega.nz/#F!J8QnxKqZ!cC6lQ9GUHZmH46CXlOBUEw'
     },
     {
         id: 3,
         name: 'הנדסת תוכנה',
-        link: ''
+        link: 'https://mega.nz/#F!hwQhXQbS!VZorn7kTOAhs3KlqbyK0uw'
     },
     {
         id: 4,
         name: 'תורת הגרפים',
-        link: ''
+        link: 'https://mega.nz/#F!V8ZxjCoD!mEDs81HT8D16yCHJ5AVHAQ'
     },
     {
         id: 5,
         name: 'תכנות מונחה עצמים',
-        link: ''
+        link: 'https://mega.nz/#F!llJT1IoD!dV8CwRGgFE5lfD3kB-CDTQ'
     },
     {
         id: 6,
         name: 'רשתות תקשורת',
-        link: ''
+        link: 'https://mega.nz/#F!dgZHFA4Q!6OkN9QWfN3zMVxdL-mGP5Q'
     },
     {
         id: 7,
         name: 'מערכות הפעלה',
-        link: ''
+        link: 'https://mega.nz/#F!A9A3XSwa!BUrzTj0QZW3YTy2TFq3aSQ'
     },
     {
         id: 8,
         name: 'ניתוח אלגוריתמים',
-        link: ''
+        link: 'https://mega.nz/#F!p5YnwAZA!2oScWLwWScbP0Mb4bE5ffg'
     },
     {
         id: 9,
         name: 'אוטומטים',
-        link: ''
+        link: 'https://mega.nz/#F!YhIzSYRR!NHGF7N4tU1MyVpFmXydkcw'
     },
     {
         id: 10,
         name: '*לוגיקה',
-        link: ''
+        link: 'https://mega.nz/#F!YoQD1IbR!iXQm2pGDOgZ61jc4TI2Mog'
     },
 ];
 
@@ -103,27 +103,27 @@ export const thirdYearModules = [
     {
         id: 1,
         name: 'כלים מתמטיים',
-        link: ''
+        link: 'https://mega.nz/#F!JtJzFQSK!euSLFyG4KEFG6-nwr2ImCg'
     },
     {
         id: 2,
         name: 'חישוביות וסיבוכיות',
-        link: ''
+        link: 'https://mega.nz/#F!otIHTYxJ!-cN4DhfytXljWJWq1CwtXw'
     },
     {
         id: 3,
         name: 'קומפילציה',
-        link: ''
+        link: 'https://mega.nz/#F!hsQTVCZA!Bm0PFXGKgCE1nBYtCoBALg'
     },
     {
         id: 4,
-        name: 'כלים מתמטיים',
+        name: 'למידת מכונה - כרגע ריק',
         link: ''
     },
     {
         id: 5,
         name: '*תכנות לוגי',
-        link: ''
+        link: 'https://mega.nz/#F!UwZBRKRT!fLDtyt0bHLdZ8d6UVLdhOQ'
     },
 ];
 
@@ -131,77 +131,77 @@ export const optionalModules = [
     {
         id: 1,
         name: 'תכנות בסביבת אינטרנט',
-        link: ''
+        link: 'https://mega.nz/#F!M9JzXCiC!J-6IUUsKUcFZYEFTJt88Lg'
     },
     {
         id: 2,
         name: 'בניית יישומים מאובטחים',
-        link: ''
+        link: 'https://mega.nz/#F!xlY1gQ6A!Sy5_5cY3NRVBIkWURlT5tQ'
     },
     {
         id: 3,
         name: 'אבטחת רשתות',
-        link: ''
+        link: 'https://mega.nz/#F!EtZDXQoS!e2y5-pcSViyvT696FDCtlg'
     },
     {
         id: 4,
         name: 'כריית נתונים',
-        link: ''
+        link: 'https://mega.nz/#F!p9BjFSKK!WWshxgoFJXYIfXJqJVXxWA'
     },
     {
         id: 5,
         name: 'java צד שרת',
-        link: ''
+        link: 'https://mega.nz/#F!xwR3naBY!1sBD3Tm8PLxQcb1Q7nNHOg'
     },
     {
         id: 6,
         name: 'C#',
-        link: ''
+        link: 'https://mega.nz/#F!ppRHCI4J!1U-7GkleEn6e1w5LF7vxQQ'
     },
     {
         id: 7,
         name: 'בדיקות תוכנה',
-        link: ''
+        link: 'https://mega.nz/#F!EhYXEY6B!YYnPGPHAOrpjNEd6i9OK0Q'
     },
     {
         id: 8,
         name: 'גרפיקה ממוחשבת',
-        link: ''
+        link: 'https://mega.nz/#F!M1IRVQJI!2oWAW-EE2CFkg-9j-4gCpQ'
     },
     {
         id: 9,
         name: 'עיבוד שפה טבעית',
-        link: ''
+        link: 'https://mega.nz/#F!VtQDASJI!4k_Rsy_pGBItsqBf8afyfw'
     },
     {
         id: 10,
         name: 'בינה מלאכותית',
-        link: ''
+        link: 'https://mega.nz/#F!Z5ADwIjL!UncZAmOKKiWYIq-yK65ScQ'
     },
     {
         id: 11,
         name: 'רובוטיקה',
-        link: ''
+        link: 'https://mega.nz/#F!59AFWagT!ujJFHmeMc57tGp2ZL2YcPQ'
     },
     {
         id: 12,
         name: 'תבניות עיצוב',
-        link: ''
+        link: 'https://mega.nz/#F!QsQnkKCL!2pyIXBXnVnNWsNmpLuy55Q'
     },
     {
         id: 13,
-        name: 'הצפנה',
-        link: ''
+        name: 'יסודות מערכות מידע',
+        link: 'https://mega.nz/#F!g0Ax2KgA!jqFr6uKMLWASXyS65Az1Pw'
     },
     {
         id: 14,
         name: 'GIS',
-        link: ''
+        link: 'https://mega.nz/#F!BwYVQCiA!RioWAzzrSgRnO-3GGpEauw'
     },
     {
         id: 15,
         name: 'פיתוח אפלקיציות אנדרואיד',
-        link: ''
+        link: 'https://mega.nz/#F!dtAHlawY!v__CswfmsPTrEqDFYvGsZg'
     },
 ];
 
@@ -209,96 +209,96 @@ export const generalModules =[
     {
         id: 1,
         name: 'אתגרי מחשבה בפילוסופיה',
-        link: ''
+        link: 'https://mega.nz/#F!chQRwA7B!vXiBHK1LS8kGUyLq2QBFVQ'
     },
     {
         id: 2,
         name: 'אתיקה בעסקים',
-        link: ''
+        link: 'https://mega.nz/#F!IoBlzCoI!Jor7k-Z_zBpQRdydQF1Tkw'
     },
     {
         id: 3,
-        name: 'האדם בין מימוש לאחריות',
-        link: ''
+        name: 'האדם בין מימוש לאחריות חברתית',
+        link: 'https://mega.nz/#F!8wY3gIxK!0OB6hK_YY_PMGoRbtEt_CQ'
     },
     {
         id: 4,
         name: 'הבעות פנים',
-        link: ''
+        link: 'https://mega.nz/#F!g8JVmIwa!KUeX1CfPsQnqYnzRD7OwUQ'
     },
     {
         id: 5,
         name: 'הגירה בעולם משתנה',
-        link: ''
+        link: 'https://mega.nz/#F!E0IXXSDR!GjHykxtMKfZ6-EMBiLbiWA'
     },
     {
         id: 6,
         name: 'ידיעה ורגשות',
-        link: ''
+        link: 'https://mega.nz/#F!xhZBVCCY!bTfg_JCKFBt5FzRwlLSOXg'
     },
     {
         id: 7,
         name: 'יסודות התורה והקבלה',
-        link: ''
+        link: 'https://mega.nz/#F!gsBlnKbB!47UYjL2pON3Qq5U2vrWqzA'
     },
     {
         id: 8,
         name: 'יצירתן של מדינות המזרח התיכון',
-        link: ''
+        link: 'https://mega.nz/#F!c8BTyQra!FPOeLEHPv9-7UnhkTeJXrA'
     },
     {
         id: 9,
         name: 'מבוא לביולוגיה מודרנית',
-        link: ''
+        link: 'https://mega.nz/#F!VhJjyaIS!5csx1eL1Y7ap1o6b37dHgw'
     },
     {
         id: 10,
         name: 'מבוא לקיימות',
-        link: ''
+        link: 'https://mega.nz/#F!Z0YR3CKb!5WZJRH1ZMnEWecuH4VzMOA'
     },
     {
         id: 11,
         name: 'מוסיקה של שנות ה-60',
-        link: ''
+        link: 'https://mega.nz/#F!4tQBiYZY!_UXdqOo9x8U4Wtij5JPDxw'
     },
     {
         id: 12,
         name: 'סובלנות בחברה רב-תרבותית',
-        link: ''
+        link: 'https://mega.nz/#F!tsZFTKhZ!HMKLJdWexdKFP6LiI-u0vA'
     },
     {
         id: 13,
         name: 'סוגיות נבחרות בפסיכולוגיה של המנהיגות',
-        link: ''
+        link: 'https://mega.nz/#F!M4IX3aKS!TqIVZ9n9oSbHava7J1tzFA'
     },
     {
         id: 14,
         name: 'סוגיות פילוסופיות ביצירות ספרותיות',
-        link: ''
+        link: 'https://mega.nz/#F!chQxFYoR!27Ieu5GaKL9xDX_AqcsH6w'
     },
     {
         id: 15,
         name: 'פילוסופיה וקולנוע',
-        link: ''
+        link: 'https://mega.nz/#F!ItQVgA4Q!YyYQxDZXSEObTQV-PEDsQA'
     },
     {
         id: 16,
         name: 'קהילות פוליטיקה עסקים באינטרנט',
-        link: ''
+        link: 'https://mega.nz/#F!x5IHkSSA!Y_A9d8i-rGZzHesWmKHWbA'
     },
     {
         id: 17,
         name: 'קמפיינים פוליטיים',
-        link: ''
+        link: 'https://mega.nz/#F!ItYzWApI!DAx8rRylKZoM9hNludSB-w'
     },
     {
         id: 18,
         name: 'שידורי טלוויזיה בכבלים ובלווין',
-        link: ''
+        link: 'https://mega.nz/#F!Q9AxlSAb!gmiivuqZsx7aVAAg47tSdQ'
     },
     {
         id: 19,
-        name: 'נשים במקרא',
-        link: ''
+        name: 'נשים במקרא בראיה פמניסטית-סוציולוגית',
+        link: 'https://mega.nz/#F!lpYDEIIb!SJm-yvfiEhzwACudcBtETQ'
     },
 ];
