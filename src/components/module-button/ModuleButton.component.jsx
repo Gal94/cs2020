80cbc0d7e323f323513338075e0388be9ca16398
Gal94 <yacobi.gal94@gmail.com>
2019-10-05 14:42:55 +0300

    added props to ModulesButton component. added links and updated homepage text.

--- src/components/module-button/ModuleButton.component.jsx
+++ src/components/module-button/ModuleButton.component.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './ModuleButton.styles.scss'
 
-const ModuleButton = ({name , link}) => (
-    <button className='module-button' onClick={()=> {
+const ModuleButton = ({name , link, homeButton}) => (
+    <button className={`${homeButton ? 'home-link': ''} module-button`} onClick={()=> {
         window.location.href = `${link}`;
     }}> {name} </button>
 );
