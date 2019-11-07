26610024494e706c3d1e95219cc0c58806d0fe28
Gal Yacobi <37088991+Gal94@users.noreply.github.com>
2019-11-07 20:06:15 +0200

    Removed the icon prop
    Removed the icon prop due to not implementing any icons on the buttons.

--- src/components/module-button/ModuleButton.component.jsx
+++ src/components/module-button/ModuleButton.component.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './ModuleButton.styles.scss'
 
-const ModuleButton = ({name , link, homeButton, icon}) => (
+const ModuleButton = ({name , link, homeButton}) => (
     <button className={`${homeButton ? 'home-link': ''} module-button`} onClick={()=> {
         window.location.href = `${link}`;
-}}>{name}{icon ? (<span style={{marginRight: `0.2rem`}}>{icon}</span>):null} </button>
+    }}>{name}</button>
 );
 
 export default ModuleButton;
