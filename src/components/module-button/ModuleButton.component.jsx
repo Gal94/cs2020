58ae5fe930cc9789899622800ad384e9f2838e2d
Gal94 <yacobi.gal94@gmail.com>
2019-11-07 20:56:14 +0200

    Revert "Removed the icon prop"
    This reverts commit 26610024494e706c3d1e95219cc0c58806d0fe28.

--- src/components/module-button/ModuleButton.component.jsx
+++ src/components/module-button/ModuleButton.component.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './ModuleButton.styles.scss'
 
-const ModuleButton = ({name , link, homeButton}) => (
+const ModuleButton = ({name , link, homeButton, icon}) => (
     <button className={`${homeButton ? 'home-link': ''} module-button`} onClick={()=> {
         window.location.href = `${link}`;
-    }}>{name}</button>
+}}>{name}{icon ? (<span style={{marginRight: `0.2rem`}}>{icon}</span>):null} </button>
 );
 
 export default ModuleButton;
